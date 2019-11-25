9dfb81feeb6733b6e9a3d5a5c6bbef32097821b6
Swetabja Hazra <swetabjahazra@Swetabjas-Awesome-Mac.local>
2019-11-26 05:03:35 +0530

    Profile details route added

--- dbschema.js
+++ dbschema.js
@@ -1,11 +1,47 @@
 let db = {
+  users: [
+    {
+      userId: 'dh23ggj5h32g543j5gf43',
+      email: 'user@email.com',
+      handle: 'user',
+      createdAt: '2019-03-15T10:59:52.798Z',
+      imageUrl: 'image/sajhvbcdshjvbdjs/sdhjvbdsjhvcs',
+      bio: 'Hello, my name is user, nice to meet you',
+      website: 'https://user.com',
+      location: 'London, UK'
+    }
+  ],
   screams: [
     {
       userHandle: 'user',
-      body: 'this is the scream body',
+      body: 'This is the scream body',
       createdAt: '2019-11-21T01:31:36.392Z',
       likeCount: 5,
-      commentCount: 2
+      commentCount: 3
+    }
+  ]
+}
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: 'dh23ggj5h32g543j5gf43',
+    email: 'user@email.com',
+    handle: 'user',
+    createdAt: '2019-03-15T10:59:52.798Z',
+    imageUrl: 'image/sajhvbcdshjvbdjs/sdhjvbdsjhvcs',
+    bio: 'Hello, my name is user, nice to meet you',
+    website: 'https://user.com',
+    location: 'London, UK'
+  },
+  likes: [
+    {
+      userHandle: 'user',
+      screamId: 'shdbcdjscbvjdsvc'
+    },
+    {
+      userHandle: 'user',
+      screamId: 'dhsgcvdvcjhdsvcj'
     }
   ]
 }
